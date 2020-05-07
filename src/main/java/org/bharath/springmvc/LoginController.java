8d390a3f75a0cf080819de8263293cf9721a893a
Bharath Kumar <bharathk494@gmail.com>
2020-05-07 13:23:17 +0530

    First spring MVC controller

---
+++ src/main/java/org/bharath/springmvc/LoginController.java
@@ -0,0 +1,17 @@
+package org.bharath.springmvc;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class LoginController 
+{
+	@RequestMapping("/login")
+	@ResponseBody
+	public String sendMessage()
+	{
+		return "Hello World";
+	}
+
+}
